16f0daf8fd144670f944da4f960f1339e9bde501
Marc Abbyad <marc@medicmobile.org>
2015-06-02 11:44:59 -0400

    Fix paths to form and instance providers. Issue #16
    Create providers in the org.medicmobile scope that use the base odk
    providers.

---
+++ src/org/medicmobile/collect/android/provider/FormsProvider.java
@@ -0,0 +1,5 @@
+package org.medicmobile.collect.android.provider;
+
+public class FormsProvider extends org.odk.collect.android.provider.FormsProvider {
+
+}
---
+++ src/org/medicmobile/collect/android/provider/InstanceProvider.java
@@ -0,0 +1,5 @@
+package org.medicmobile.collect.android.provider;
+
+public class InstanceProvider extends org.odk.collect.android.provider.InstanceProvider {
+
+}
