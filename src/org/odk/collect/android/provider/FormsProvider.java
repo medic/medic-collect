aee033c028d13a480f9c7fde1e6b8013019bdf99
Gareth Bowen <gareth@medicmobile.org>
2018-08-16 09:52:47 +1200

    Fixes SQL injection vulnerability
    medic/medic-webapp#4776

--- src/org/odk/collect/android/provider/FormsProvider.java
+++ src/org/odk/collect/android/provider/FormsProvider.java
@@ -266,14 +266,14 @@ public class FormsProvider extends ContentProvider {
 			String[] selectionArgs, String sortOrder) {
 		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
 		qb.setTables(FORMS_TABLE_NAME);
+		qb.setProjectionMap(sFormsProjectionMap);
+		qb.setStrict(true);
 
 		switch (sUriMatcher.match(uri)) {
 		case FORMS:
-			qb.setProjectionMap(sFormsProjectionMap);
 			break;
 
 		case FORM_ID:
-			qb.setProjectionMap(sFormsProjectionMap);
 			qb.appendWhere(FormsColumns._ID + "="
 					+ uri.getPathSegments().get(1));
 			break;
@@ -521,10 +521,9 @@ public class FormsProvider extends ContentProvider {
 			count = db.delete(
 					FORMS_TABLE_NAME,
 					FormsColumns._ID
-							+ "="
-							+ formId
+							+ "=?"
 							+ (!TextUtils.isEmpty(where) ? " AND (" + where
-									+ ')' : ""), whereArgs);
+									+ ')' : ""), prepareWhereArgs(whereArgs, formId));
 			break;
 
 		default:
@@ -673,10 +672,9 @@ public class FormsProvider extends ContentProvider {
 							FORMS_TABLE_NAME,
 							values,
 							FormsColumns._ID
-									+ "="
-									+ formId
+									+ "=?"
 									+ (!TextUtils.isEmpty(where) ? " AND ("
-											+ where + ')' : ""), whereArgs);
+											+ where + ')' : ""), prepareWhereArgs(whereArgs, formId));
 				} else {
 					Log.e(t, "Attempting to update row that does not exist");
 				}
@@ -694,6 +692,18 @@ public class FormsProvider extends ContentProvider {
 		getContext().getContentResolver().notifyChange(uri, null);
 		return count;
 	}
+
+    private String[] prepareWhereArgs(String[] whereArgs, String formId) {
+        String[] newWhereArgs;
+        if (whereArgs == null || whereArgs.length == 0) {
+            newWhereArgs = new String[] {formId};
+        } else {
+            newWhereArgs = new String[(whereArgs.length + 1)];
+            newWhereArgs[0] = formId;
+            System.arraycopy(whereArgs, 0, newWhereArgs, 1, whereArgs.length);
+        }
+        return newWhereArgs;
+    }
 
 	static {
 		sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
