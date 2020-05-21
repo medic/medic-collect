cc4c9b30d2505073a87986f8379c155855c6ed30
Bernard K <bernard@medicmobile.org>
2020-05-21 16:48:03 +0300

    add cdc-mohke EBS dev flavours & HEBS apks

---
+++ run.sh
@@ -0,0 +1,8 @@
+# ./gradlew --daemon --parallel assembleCdcmohkedsruchaDebug
+# ./gradlew --daemon --parallel assembleCdcmohkedsruscdscDebug
+./gradlew --daemon --parallel assembleCdcmohkedsrudevDebug
+./gradlew --daemon --parallel assembleCdcmohkedsruchadevDebug
+./gradlew --daemon --parallel assembleCdcmohkedsruscdscdevDebug
+# ./gradlew --daemon --parallel assembleCdcmohkedsruhebsdevDebug
+# ./gradlew --daemon --parallel assembleCdcmohkedsruhebsfpDebug
+# ./gradlew --daemon --parallel assembleCdcmohkedsruhebsscdscDebug
