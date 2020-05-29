2649f4daa51b263679070fef8abf4ac920967131
Bernard K <bernard@medicmobile.org>
2020-05-29 15:26:50 +0300

    Rename mdharura flavors.

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-# ./gradlew --daemon --parallel assembleCdcmohkedsruchaDebug
-# ./gradlew --daemon --parallel assembleCdcmohkedsruscdscDebug
-./gradlew --daemon --parallel assembleCdcmohkedsrudevDebug
-./gradlew --daemon --parallel assembleCdcmohkedsruchadevDebug
-./gradlew --daemon --parallel assembleCdcmohkedsruscdscdevDebug
-# ./gradlew --daemon --parallel assembleCdcmohkedsruhebsdevDebug
-# ./gradlew --daemon --parallel assembleCdcmohkedsruhebsfpDebug
-# ./gradlew --daemon --parallel assembleCdcmohkedsruhebsscdscDebug
+./gradlew --daemon --parallel assemblemdharurachaDebug
+./gradlew --daemon --parallel assemblemdharurascdscDebug
+./gradlew --daemon --parallel assembleMdharuradevDebug
+./gradlew --daemon --parallel assembleMdharurachadevDebug
+./gradlew --daemon --parallel assembleMdharurascdscdevDebug
+./gradlew --daemon --parallel assembleMdharurahebsdevDebug
+./gradlew --daemon --parallel assembleMdharurahebsfpDebug
+./gradlew --daemon --parallel assembleMdharurahebsscdscDebug
